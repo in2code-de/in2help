d1137826aa749b5fbacd344f90d9c96d7e59e4e4
martin <martin.huber@in2code.de>
2014-02-07 09:54:55 +0100

    Initialer Commit - Umbau der in2tumhelp in eine neutrale Extension um projektunabhängig benutzt werden zu können.

---
+++ ext_tables.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+if (TYPO3_MODE === 'BE') {
+
+	/**
+	 * Registers a Backend Module
+	 */
+	Tx_Extbase_Utility_Extension::registerModule(
+		$_EXTKEY,
+		'help',	 				// Make module a submodule of 'help'
+		'm1',					// Submodule key
+		'',						// Position
+		array(
+			'Help' => 'index'
+		),
+		array(
+			'access' => 'user,group',
+			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
+			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_in2help.xml',
+		)
+	);
+
+}
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Online-Documentation');
+
+?>
