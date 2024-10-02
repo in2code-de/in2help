e9efe41a2d58e741682ca1af893413a2468b1952
Alexander Kellner <alexander.kellner@in2code.de>
2024-10-02 14:08:51 +0200

    [BUGFIX] Make module accessible for editors
    After this commit the module can be selected in user groups to be allowed to be accessed

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -3,13 +3,16 @@
 return [
     'web_in2help' => [
         'parent' => 'help',
-        'position' => '',
-        'access' => 'user, group',
+        'position' => [],
+        'access' => 'user',
         'iconIdentifier' => 'in2help',
         'labels' => 'LLL:EXT:in2help/Resources/Private/Language/locallang_in2help.xlf',
         'path' => '/module/help/m1',
         'extensionName' => 'In2help',
         'controllerActions' => [
-            \In2code\In2help\Controller\HelpController::class => 'index']
+            \In2code\In2help\Controller\HelpController::class => [
+                'index',
+            ],
+        ],
     ],
 ];
